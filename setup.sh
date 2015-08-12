d7b9c8e04201f694fe7e4ff08a99a6328680c950
root <ggoodman@gmail.com>
2015-08-12 15:01:24 +0000

    Add user's own public key

--- setup.sh
+++ setup.sh
@@ -9,11 +9,16 @@ if [ ! -f id_rsa.pub ]; then
 	ssh-keygen -t rsa -b 4096 -C "$email" -f ./id_rsa -N ''
 fi
 
-if [ ! -f c9_rsa.pub ]; then
+if [ ! -f authorized_keys ]; then
 	echo -n "Copy and paste the public key provided by Cloud9IDE's ssh workspace setup modal and press [ENTER] "
 	read authkey
 
 	echo $authkey >> ./authorized_keys
+
+	echo -n "Paste your own public key (~/.ssh/id_rsa.pub) and press [ENTER] "
+	read pubkey
+
+	echo $pubkey >> ./authorized_keys
 fi
 
 mkdir -p ~/workspace
