0cf9177e5221ca94fa4be4a81f999c86fd91d080
root <ggoodman@gmail.com>
2015-08-20 20:59:23 +0000

    Automatically set up public key on Github

--- setup.sh
+++ setup.sh
@@ -1,27 +1,42 @@
 #!/bin/bash
 
+set -eux
+
 echo "Setting up Cloud9IDE ssh workspace in CoreOS"
 
-if [ ! -f ~/.ssh/id_rsa.pub ]; then
-	echo -n "Enter the email address to use in generating your ssh key and press [ENTER]"
-	read email
+mkdir -p ./.ssh
+
+if [ ! -f ./.ssh/id_rsa.pub ]; then
+	read -e -p "Enter the email address to use in generating your ssh key and press [ENTER]: " EMAIL
 	
-	mkdir -p ./.ssh
-	ssh-keygen -t rsa -b 4096 -C "$email" -f ./.ssh/id_rsa -N ''
+	ssh-keygen -t rsa -b 4096 -C "$EMAIL" -f ./.ssh/id_rsa -N ''
+	
+	echo
+	echo "Adding the public key just generated to your Github profile..."
+	
+	read -e -p "Enter the name that will identify this public key on Github and press [ENTER]: " -i "Cloud9@$HOST" GH_KEY_TITLE
+	read -e -p "Enter your github username and press [ENTER]: " GH_USER
+	read -e -s -p "Enter your github password and press [ENTER]: " GH_PASS
+	
+	curl -XPOST -u $GH_USER:$GH_PASS -H "Content-Type: application/json" --data-binary @- https://api.github.com/user/keys <<EOF
+{
+	"title": "$GH_KEY_TITLE",
+	"key":"$(cat ./.ssh/id_rsa.pub)"
+}
+EOF
+
 fi
 
 if [ ! -f ./.ssh/authorized_keys ]; then
-	echo -n "Copy and paste the public key provided by Cloud9IDE's ssh workspace setup modal and press [ENTER] "
-	read authkey
+	read -e -p "Copy and paste the public key provided by Cloud9IDE's ssh workspace setup modal and press [ENTER]: " C9_PUB_KEY
 
 	echo "# Cloud9IDE public key" >> ./.ssh/authorized_keys
-	echo $authkey >> ./.ssh/authorized_keys
+	echo $C9_PUB_KEY >> ./.ssh/authorized_keys
 
-	echo -n "Paste your own public key (~/.ssh/id_rsa.pub) and press [ENTER] "
-	read pubkey
+	read -e -p "Paste your own public key (~/.ssh/id_rsa.pub) and press [ENTER]: " USER_PUB_KEY
 
 	echo "# User's public key" >> ./.ssh/authorized_keys
-	echo $pubkey >> ./.ssh/authorized_keys
+	echo $USER_PUB_KEY >> ./.ssh/authorized_keys
 fi
 
 mkdir -p ~/workspace
